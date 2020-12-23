59febdacae2a41b99517574f82733d4329030b60
SourabhaDixit <70750867+SourabhaDixit@users.noreply.github.com>
2020-12-23 23:37:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,124 +1,96 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
- 
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+session_start();//starting session
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
+//if session exit,user neither need to sign nor need to signup
+if(isset($_SESSION['login_id'])) {
+	if(isset($_SESSION['pageStore'])){
+		$pagestore=$_SESSION['pageStore'];
+		header("location:$pageStore");//redirecting to profile page
     }
-    
-    // Close connection
-    mysqli_close($link);
 }
+
+//Login procress start, if user press the signin button
+if(isset($_POST['signIn'])) {
+	if(empty($_POST['email']) || empty($_POST['password'])){
+		echo "Username and passoord should not be empty";
+	}
+	else{
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		//Make a connection with MYSOL server
+		include('config.php');
+
+		$sQuery = "SELECT id, password from account where email=? LIMIT 1";
+
+		//To protect fron MYSQL injection foe a security purpose
+		$stmt = $conn->prepare($sQuery);
+		$stmt->bind_param("s", $email);
+		$stmt->execute();
+		$stmt->bind_result($id, $hash);
+		$stmt->store_result();
+
+		if($stmt->fetch()){
+			if(password_verify($password, $hash)){
+				$_SESSION['login_id'] = $id;
+
+				if(isset($_SESSION['pageStore'])){
+					$pageStore = $_SESSION['pagestore'];
+				}
+				else{
+					$pageStore = "index.php";
+				}
+				header("location: $pageStore");//Redirecting to profile page
+				$stmt->close();
+				$conn->close();
+			}
+			else{
+				echo "Invalid Username and Password";
+			}
+		}
+		else {
+			echo "Invalid Username and Password";
+		}
+		$stmt->close();
+		$conn->close();
+
+		}  
+	}
 ?>
- 
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=`1">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="rlform.css">
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
-    </div>    
+	<div class="rlform">
+		<div class="rlform rlform-wrapper">
+			<div class="rlform-box">
+				<div class="rlform-box-inner">
+					<form method="post">
+						<p>Signin to Continue</p>
+
+						<div class="rlform-group">
+							<label>Email</label>
+							<input type="email" name="email" class="rlform-input" required>
+						</div>
+						<div class="rlform-group">
+							<label>Password</label>
+							<input type="password" name="password" class="rlform-input" required>
+						</div>
+						
+						<button type="submit" class="rlform-btn" name="signIn">Sign In</button>
+						<div class="r\text-foot">
+							Dont have an account? <a href="register.php">Register</a>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
