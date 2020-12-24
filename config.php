8e32578bd8dfdb77633658484a7d66f762f96276
SourabhaDixit <70750867+SourabhaDixit@users.noreply.github.com>
2020-12-24 09:14:11 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname ="test";
+
+//create a database connection
+$conn = new mysqli($host , $dbusername , $dbpassword, $dbname);
+?>
